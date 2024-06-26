8577bfeab858c5f64e315ba0bf322771165dda3c
Will <deniswillianpereira02@gmail.com>
2024-06-26 19:27:49 -0300

    commit#4

--- Conta Banco/src/ContaTerminal.java
+++ Conta Banco/src/ContaTerminal.java
@@ -1,10 +1,26 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        String nomeCliente = "Denis Willian";
-        String agencia = "000-0";
-        int numeroConta = 00;
-        double saldo = 1.0;
 
-        System.out.println();
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //criando o ojeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome abaixo:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite abaixo o numero de sua agencia:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite abaixo o numero de sua conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Seu saldo ataul é de:");
+        double saldo = scanner.nextDouble();
+
+        //frase de impreção
+        System.out.println("Ola " + nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia + ", conta " +numeroConta + " e seu saldo " +saldo + "já está disponivél para saque.");
+        
     }
 }
