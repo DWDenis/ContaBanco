ab3c0513fb82f19e8fe9981f165ca194cb658760
Will <deniswillianpereira02@gmail.com>
2024-06-26 18:18:21 -0300

    commit#1

---
+++ Conta Banco/src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        String nomeCliente = "Denis Willian";
+        String agencia = "000-0";
+        int numeroConta = 00;
+        double saldo = 1.0;
+
+        System.out.println();
+    }
+}
